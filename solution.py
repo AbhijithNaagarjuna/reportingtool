d620e171c8f3d74477b4c44244ec2bbdc3330e76
Abhijith Naagarjuna <nagarjunaabhijith@gmail.com>
2019-07-20 15:10:23 +0530

    ADDED COMMENTS

--- solution.py
+++ solution.py
@@ -1,9 +1,14 @@
+#THE FOLLOWING VIEW TABLES HAVE BEEN USED
+#LOGD - VIEW TABLE
+#AUTHORD - VIEW TABLE
+
+
 import psycopg2
 import numpy as np
 import re
 from datetime import datetime
 
-
+#CODE TO FIND OUT THE MOST ERRORS OCCURED ON A PARTICULAR DAY
 print("The most errors occured on : ")
 db = psycopg2.connect("dbname=news")
 c = db.cursor()
@@ -12,10 +17,10 @@ t = c.fetchall()
 res = datetime.strftime(t[0][0],'%b %d, %Y')
 print(res)
 db.close()
-
 print("")
 print("The most viewed articles are :")
 
+#CODE TO FIND OUT THE MOST VIEWED ARTICLES
 db = psycopg2.connect("dbname=news")
 c = db.cursor()
 c.execute(" select articles.title,logd.views from articles,logd where articles.slug = logd.substring order by views desc limit 5")
@@ -28,6 +33,7 @@ db.close()
 print("")
 print("The most popular Authors are")
 
+#CODE TO FIND OUT THE MOST POPULAR AUTHORS
 db = psycopg2.connect("dbname=news")
 c = db.cursor()
 c.execute("select authors.name,sum(authord.views) from authors,authord where authord.author = authors.id group by name order by sum desc")
